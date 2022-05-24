0da6586d3632d023780311f8bad10ad343807f8f
SDasty <103962485+SDasty@users.noreply.github.com>
2022-05-23 20:27:35 -0500

    Add files via upload

--- Fundamentos proyecto 2/Metodos.java
+++ Fundamentos proyecto 2/Metodos.java
@@ -1,9 +1,17 @@
 public class Metodos{
-public static void prueba(){
-    
-    System.out.println(Dato.getPrecipitacion());   
-    
+public void DesviacionEstandar(double tmax, double tmin){
+    double sum = 0.0;
+    int sumatoria = 0;
+    for(int i=0, i<this.Dato.tmax.length(); i++){
+        if this.Dato.tmax.length(i).get(tmax){
+            suma += tmax;
+            this.Dato.tmax.length(i).get(tmax))*Dato.tmax.length(i).get(tmax)
+            sum++;
+        }
+    }System.out.println("math.sqrt(sum/sumatioria));
 }
+}
+
 }
 
 
